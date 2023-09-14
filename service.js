3df6adae0cee5070f25a66fe5c3f1027996b545c
asahi-gigas <asahi@gigas-jp.com>
2023-09-14 11:26:41 +0630

    fix service file

--- service.js
+++ service.js
@@ -35,7 +35,7 @@ self.addEventListener("install", async () => {
     const options = { applicationServerKey, userVisibleOnly: true };
     const subscription = await self.registration.pushManager.subscribe(options);
     // const response = await saveSubscription(subscription);
-    console.log('subscription ', subscription);
+    console.log('subscription ', JSON.stringify(subscription));
   } catch (err) {
     console.log("Error", err);
   }
